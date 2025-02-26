b140235936d587e974e30950858ecf8f6e5ae566
Daniel-Platz <dani0005@stud.kea.dk>
2025-02-26 10:31:09 +0100

    Initial commit

---
+++ src/main/java/org/example/qodanagithubactions/QodanaGitHubActionsApplication.java
@@ -0,0 +1,13 @@
+package org.example.qodanagithubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QodanaGitHubActionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QodanaGitHubActionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/qodanagithubactions/QodanaGitHubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.qodanagithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QodanaGitHubActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
